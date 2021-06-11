4c379076dbbc188d290d7ae22483ccb0fb5f65c4
Maksym Havrylov <m.havrylov001@gmail.com>
2021-06-11 21:15:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuCloseButton = document.querySelector('.menu-close');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.add('active');
+  menuCloseButton.classList.add('active');
+});
+
+menuCloseButton.addEventListener('click', () => {
+  menu.classList.remove('active');
+  menuCloseButton.classList.remove('active');
+});
