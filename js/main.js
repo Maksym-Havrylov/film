b47174c430436c7a72e4ab501adb5c14ca5e1521
Maksym Havrylov <salomondr007@gmail.com>
2021-06-15 22:36:07 +0300

    premier.html

--- js/main.js
+++ js/main.js
@@ -11,3 +11,47 @@ menuCloseButton.addEventListener('click', () => {
   menu.classList.remove('active');
   menuCloseButton.classList.remove('active');
 });
+
+//comming soon
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock(){
+    var t = getTimeRemaining(endtime);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock,1000);
+}
+
+// LAUNCH DATE
+// USE CHRISTMAS DAY so countdown is not a negative value for foreseeable future
+var deadline = 'December 25 2016 00:00:00 UTC';
+initializeClock('countdown', deadline); //comming soon *END*
