f655a07c7caa1aedc7a94f2f75aff4071faab758
Maksym Havrylov <salomondr007@gmail.com>
2021-07-07 20:52:24 +0300

    slider done

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,89 @@
-//slick-slider
-$('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
+$(document).ready(function () {
+  var zindex = 10;
 
-$(".slider").slick({
+  $("div.card").click(function (e) {
+    e.preventDefault();
 
-  // normal options...
-  infinite: false,
+    var isShowing = false;
 
-  // the magic
-  responsive: [{
+    if ($(this).hasClass("show")) {
+      isShowing = true
+    }
 
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true
+    if ($("div.cards").hasClass("showing")) {
+      // a card is already in view
+      $("div.card.show")
+        .removeClass("show");
+
+      if (isShowing) {
+        // this card was showing - reset the grid
+        $("div.cards")
+          .removeClass("showing");
+      } else {
+        // this card isn't showing - get in with it
+        $(this)
+          .css({
+            zIndex: zindex
+          })
+          .addClass("show");
+
       }
 
-    }, {
+      zindex++;
 
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
+    } else {
+      // no cards in view
+      $("div.cards")
+        .addClass("showing");
+      $(this)
+        .css({
+          zIndex: zindex
+        })
+        .addClass("show");
+
+      zindex++;
+    }
+
+  });
+});
+
+
+
+jQuery(document).ready(function ($) {
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-
-    }, {
-
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
-
-    }]
-}); //slick-slider *END*
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
